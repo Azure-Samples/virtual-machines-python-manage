af3cc237de9837c6041cf88c3269e008e60244fa
Ian McCowan <v-iam@microsoft.com>
2017-06-28 14:48:07 -0700

    Extract credentials to a function

--- example.py
+++ example.py
@@ -59,17 +59,22 @@ VM_REFERENCE = {
 }
 
 
-def run_example():
-    """Virtual Machine management example."""
-    #
-    # Create all clients with an Application (service principal) token provider
-    #
+def get_credentials():
     subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
     credentials = ServicePrincipalCredentials(
         client_id=os.environ['AZURE_CLIENT_ID'],
         secret=os.environ['AZURE_CLIENT_SECRET'],
         tenant=os.environ['AZURE_TENANT_ID']
     )
+    return credentials, subscription_id
+
+
+def run_example():
+    """Virtual Machine management example."""
+    #
+    # Create all clients with an Application (service principal) token provider
+    #
+    credentials, subscription_id = get_credentials()
     resource_client = ResourceManagementClient(credentials, subscription_id)
     compute_client = ComputeManagementClient(credentials, subscription_id)
     network_client = NetworkManagementClient(credentials, subscription_id)
